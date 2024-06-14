86c9d5db37e522730b0864c287065078306ff553
thijs2163Test <thijsdammers@gmail.com>
2024-06-14 11:35:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,13 +15,9 @@ session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
 
 # Convert Snowpark DF to Pandas DF
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
 
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write('The name on your Smoothie will be: ', name_on_order)
